311a50d5d444db09c2f7d06cfc6fe4bf5ffe126b
Nate Schultz <ngs32@drexel.edu>
2021-05-01 20:33:10 -0400

    prob 1 done

--- prob_1/main.sol
+++ prob_1/main.sol
@@ -2,7 +2,18 @@
 pragma solidity ^0.8.4;
 
 contract Main {
-    function main() public {
-        
+    // If we list all the natural numbers below 10 that are multiples of 3 or 5,
+    // we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    // Find the sum of all the multiples of 3 or 5 below 1000.
+
+    uint ret = 0;
+
+    function main() public returns (uint) {
+        for(uint i=0; i < 1000; i++) {
+            if(i % 3 == 0 || i % 5 == 0) {
+                ret += i;
+            }
+        }
+        return ret;
     }
 }
